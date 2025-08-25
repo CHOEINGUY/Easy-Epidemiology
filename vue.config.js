bc379b670b478ad9954f7d9bdad0520dcbe41131
Choeingyu <chldlsrb07@gmail.com>
2025-08-25 16:23:24 +0900

    v1.4

--- vue.config.js
+++ vue.config.js
@@ -28,7 +28,7 @@ module.exports = defineConfig({
     config
       .plugin('html')
       .tap(args => {
-        args[0].title = 'Easy-Epidemiology Web v1.3';
+        args[0].title = 'Easy-Epidemiology Web v1.4';
         // 오프라인 실행을 위한 메타 태그 추가
         args[0].meta = {
           ...args[0].meta,
